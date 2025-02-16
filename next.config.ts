a93ee73643cab1bf61321b4a20077e74678bfe95
André Lucas <apglu.26@gmail.com>
2025-02-16 17:25:10 -0300

    Adiciona react-icons como dependência

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,13 @@
-import type { NextConfig } from 'next'
- 
-const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  webpack: (config:any) => {
+    config.resolve.fallback = { fs: false, path: false };
+    return config;
+  },
   devIndicators: {
     appIsrStatus: false,
   },
-}
- 
-export default nextConfig
+};
+
+export default nextConfig;
