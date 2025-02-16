f1c85bb17181e0f80e72e19771c5fe6780d8a1f8
André Lucas <apglu.26@gmail.com>
2025-02-16 17:07:12 -0300

    Primeiro Commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,95 @@
-import Image from "next/image";
+"use client"
+import { useState } from "react";
+import { IoLogoInstagram } from "react-icons/io";
+import { FaLinkedinIn } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
+
+type Tarefas = {
+  id: number,
+  texto: string;
+}
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+  const [tarefa, setTarefa] = useState<string>("")
+  const [tarefas, setTarefas] = useState<Tarefas[]>([])
+
+  function Adicionar_tarefas() {
+    if (tarefa.trim() === "") {
+      alert("Por favor, insira uma tarefa!");
+      return;
+    }    
+
+    setTarefas((prev) => [...prev, { id: Date.now(), texto: tarefa }]);
+    setTarefa(""); 
+  }
+  return (
+    <div className="flex justify-center min-h-screen mt-20 px-4">
+
+      <div className="flex flex-col border-2 p-4 w-full max-w-lg rounded-xl mb-40 bg-white shadow-lg">
+
+        <div className="w-full">
+          <h1 className="text-center font-bold text-2xl sm:text-3xl mb-4">Lista de Tarefas</h1>
+          <div className="flex flex-col sm:flex-row items-center border rounded-lg p-2 shadow-md bg-white gap-2">
+  <input 
+    value={tarefa} 
+    type="text" 
+    onChange={(e) => setTarefa(e.target.value)} 
+    placeholder="Digite sua tarefa" 
+    className="flex-1 px-4 py-2 border rounded-lg placeholder:text-gray-500 text-gray-800 focus:outline-none focus:ring-2 focus:ring-purple-500"
+  />
+  <button 
+    onClick={Adicionar_tarefas} 
+    className="bg-gradient-to-br from-purple-800 to-blue-400 px-6 py-2 rounded-lg font-semibold text-white hover:scale-105 hover:shadow-lg transition-transform"
+  >
+    Adicionar
+  </button>
+</div>
+
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
+
+        <ul className="gap-2 flex flex-col mt-4">
+          {tarefas.map((item) => (
+            <li key={item.id} className="w-full border rounded-lg p-2 shadow-md bg-gray-50">
+            <div className="flex flex-col sm:flex-row justify-between items-center gap-2">
+              <input 
+                type="text" 
+                placeholder={item.texto} 
+                className="flex-1 px-2 py-2 rounded-lg border focus:outline-none focus:ring-2 focus:ring-purple-500 placeholder:text-gray-500 text-gray-800"
+              />
+              <button 
+                onClick={() => setTarefas((prev) => prev.filter((t) => t.id != item.id))} 
+                className="bg-red-500 text-white px-6 py-2 rounded-lg font-semibold hover:bg-red-600 transition-all"
+              >
+                Deletar
+              </button>
+            </div>
+          </li>   
+          ))}
+        </ul>
+      </div>
+
+      <footer className="w-full fixed bottom-0">
+        <div className="bg-black text-white p-4 border-t-2 border-gray-600">
+          <h1 className="text-center font-bold text-2xl">@DESENVOLVIDO POR ANDRÉ LUCAS</h1>
+          <div className="flex justify-center space-x-6 mt-4">
+            <a 
+              href="https://www.instagram.com/andrelucas___/"    
+              className="hover:scale-110 transition-transform">
+              <IoLogoInstagram size={28} />
+            </a>
+            <a href="https://github.com/Andrelcs12"  
+              className="hover:scale-110 transition-transform">
+              <FaGithub size={28} />
+            </a>
+            <a href="https://www.linkedin.com/in/andrelucasdev" 
+              className="hover:scale-110 transition-transform">
+              <FaLinkedinIn size={28} />
+            </a>
+          </div>
+        </div>
       </footer>
+
     </div>
   );
 }
